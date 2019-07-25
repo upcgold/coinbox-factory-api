3e2d105503a83ac381c45ee88a9a7279e29132d4
tenten <noreply@noreply.net>
2019-07-24 17:33:36 -0700

    basic card is being returned.  now need pic to go along

--- routes/matric.js
+++ routes/matric.js
@@ -30,12 +30,38 @@ router.post('/add', function(req, res, next) {
     "f": "heart"
   }
 
-  var hash = md5(req.body.matric_value);
-  var searchNum = hash.substr(0,1)
-  var suit = suits[searchNum];
-  var card = {
-    "suit" : suit
+  var numbers = {
+    "0": "2",
+    "1": "3",
+    "2": "4",
+    "3": "5",
+    "4": "6",
+    "5": "7",
+    "6": "8",
+    "7": "9",
+    "8": "10",
+    "9": "J",
+    "a": "Q",
+    "b": "K",
+    "c": "A",
+    "d": "2",
+    "e": "7",
+    "f": "A"
   }
+
+
+  var hash = md5(req.body.matric_value);
+  var searchSuite = hash.substr(0,1);
+  var searchNum = hash.substr(-1);;
+
+  var suit = suits[searchSuite];
+  var number = numbers[searchNum];
+
+
+  var card = {
+    "suit" : suit,
+    "number": number
+  };
   
   res.send(card);
 
