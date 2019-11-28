b63262f04b925a85f3f631ec8c2148c4b161e129
tenten <noreply@noreply.net>
2019-11-27 23:08:52 -0800

    updating videos for each candidate

--- candihash/routes/election.js
+++ candihash/routes/election.js
@@ -22,7 +22,7 @@ router.get('/candidate/', function (req, res, next) {
       "storeUrl":"harris.2020.codes",
       "img":"https://image.cnbcfm.com/api/v1/image/105867117-1556049501630rtx6svfw.jpg?v=1556049563&w=678&h=381",
       "videos": {
-        "economy": "w5RWUolzwWU"//youtube vid id
+        "economy": "Ls7OSwHMoBc"//youtube vid id
       }
   }
 
@@ -32,7 +32,7 @@ router.get('/candidate/', function (req, res, next) {
     "storeUrl":"sanders.2020.codes",
     "img":"https://cbsnews3.cbsistatic.com/hub/i/r/2019/10/23/16275a12-988d-496b-845a-7ac47b2f6c4a/thumbnail/1200x630/08a8c174b7ad074a7d623866c51cc413/cbsn-fusion-will-congresswoman-rashida-tlaib-endorse-bernie-sanders-thumbnail-382979-640x360.jpg",
     "videos": {
-      "economy": "w5RWUolzwWU"//youtube vid id
+      "economy": "2nwRiuh1Cug"//youtube vid id
     }
   }
 
@@ -42,7 +42,7 @@ router.get('/candidate/', function (req, res, next) {
       "storeUrl":"warren.2020.codes",
       "img":"https://iadsb.tmgrup.com.tr/612016/645/344/1/161/800/587?u=https://idsb.tmgrup.com.tr/2018/12/31/us-democrat-elizabeth-warren-enters-2020-presidential-race-1546281406284.jpg",
       "videos": {
-        "economy": "w5RWUolzwWU"//youtube vid id
+        "economy": "0iEnYYfxtRw"//youtube vid id
       }
     }
 
@@ -52,7 +52,7 @@ router.get('/candidate/', function (req, res, next) {
     "storeUrl":"warren.2020.codes",
     "img":"https://cdn.cnn.com/cnnnext/dam/assets/191119201838-10-pete-buttigieg-lead-image-large-169.jpg",
     "videos": {
-      "economy": "w5RWUolzwWU"//youtube vid id
+      "economy": "q698GWtN_wA"//youtube vid id
     }
   }
 
@@ -61,7 +61,7 @@ router.get('/candidate/', function (req, res, next) {
       "storeUrl":"yang.2020.codes",
       "img":"https://s.abcnews.com/images/Politics/andrew-yang-file-01-rtr-jc-190514_hpMain_16x9_992.jpg",
       "videos": {
-        "economy": "w5RWUolzwWU"//youtube vid id
+        "economy": "EgQb2NNQ43w"//youtube vid id
       }
     }
 
@@ -71,7 +71,7 @@ router.get('/candidate/', function (req, res, next) {
       "storeUrl":"biden.2020.codes",
       "img":"https://cdn.theatlantic.com/assets/media/img/mt/2019/04/RTX6T48T/lead_720_405.jpg?mod=1556212049",
       "videos": {
-        "economy": "w5RWUolzwWU"//youtube vid id
+        "economy": "hggux80pCWw"//youtube vid id
       }  
     }
 
@@ -80,7 +80,7 @@ router.get('/candidate/', function (req, res, next) {
     "storeUrl":"klobuchar.2020.codes",
     "img":"https://static01.nyt.com/images/2019/09/10/us/politics/10-klobuchar-candidatepage/10-klobuchar-candidatepage-facebookJumbo.jpg",
     "videos": {
-      "economy": "w5RWUolzwWU"//youtube vid id
+      "economy": "vghXHCAGcIw"//youtube vid id
     }
   }
 
@@ -89,7 +89,7 @@ var booker = {
   "storeUrl":"booker.2020.codes",
   "img":"https://miro.medium.com/max/4096/1*zUx6JY4uxY4wfnyzn1Cfuw.jpeg",
   "videos": {
-    "economy": "w5RWUolzwWU"//youtube vid id
+    "economy": "ORxLHXV7gOQ"//youtube vid id
   }
 }
 
@@ -98,7 +98,7 @@ var gabbard = {
   "storeUrl":"gabbard.2020.codes",
   "img":"https://www.newstatesman.com/sites/default/files/styles/cropped_article_image/public/blogs_2019/10/gettyimages-1173866046.jpg?itok=9ZDfbFd2&c=84d30345b25300b88238c0e74ec8a23d",
   "videos": {
-    "economy": "w5RWUolzwWU"//youtube vid id
+    "economy": "FPi4VAJtPL4"//youtube vid id
   }
 }
 
@@ -107,7 +107,7 @@ var steyer = {
   "storeUrl":"steyer.2020.codes",
   "img":"https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2019/10/screen_shot_2019-10-13_at_2.35.21_pm.png",
   "videos": {
-    "economy": "w5RWUolzwWU"//youtube vid id
+    "economy": "GXl8vRmLeJk"//youtube vid id
   }
 }
 
@@ -117,7 +117,7 @@ var castro = {
   "storeUrl":"castro.2020.codes",
   "img":"https://cdn.cnn.com/cnnnext/dam/assets/190717133057-01b-julian-castro-restricted-large-169.jpg",
   "videos": {
-    "economy": "w5RWUolzwWU"//youtube vid id
+    "economy": "LtY0SZPMIT0"//youtube vid id
   }
 }
 
@@ -126,7 +126,7 @@ var bloomberg = {
   "storeUrl":"bloomberg.2020.codes",
   "img":"https://compote.slate.com/images/2bec7c0b-5913-4ffc-9d72-8cfca894f27d.jpeg?width=780&height=520&rect=5137x3425&offset=0x277",
   "videos": {
-    "economy": "w5RWUolzwWU"//youtube vid id
+    "economy": "j_1T_xPpAwo"//youtube vid id
   }
 }
 
