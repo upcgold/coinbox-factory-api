e7076ec93b05cf97217b720817923ed1c8a085ad
Your Name <you@example.com>
2020-06-06 21:41:21 +0000

    new route.  routed the index through xham-vids

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/xhamster-videos', indexRouter);
 app.use('/candidate', candidateRouter);
 app.use('/election', electionRouter);
 
